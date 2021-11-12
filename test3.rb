a3f25f2fa91ca367492661729463cdf8f9d763c5
denisjovic <denisjovic@pm.me>
2021-11-12 16:28:49 +0100

    Added intra file sqli

--- test3.rb
+++ test3.rb
@@ -2,7 +2,9 @@ require './db.rb'
 require './query.rb'
 
 def intra_file_sql_injection
-    user = User.where("name = '#{$query}'")
+    db = $db
+    query = $query
+    user = User.where("name = '#{query}'")
 end
 
 intra_file_sql_injection()
