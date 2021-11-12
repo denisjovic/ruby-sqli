5fba75fe65fa67b0cf14ca6f2a97fd676f367a38
denisjovic <denisjovic@pm.me>
2021-11-12 16:02:12 +0100

    added new examples

--- test1.rb
+++ test1.rb
@@ -2,5 +2,11 @@ require 'sqlite3'
 
 db = SQLite3::Database.open 'test.db'
 
-user = User.where("id = '#{params[:user][:id]}'")[0]
+get_user = User.where("id = '#{params[:user][:id]}'")[0]
+
+user2 = params[:user][:id]
+
+get_user_2 = User.where("id = '#{user2}'")
+
+
 
